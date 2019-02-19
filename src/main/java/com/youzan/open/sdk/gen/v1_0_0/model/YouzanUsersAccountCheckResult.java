5a1b346bc9170fd8133b809d8cd0efaf16f06446
flyinke <flyingkeke@qq.com>
2019-02-19 17:35:41 +0800

    YouzanUsersAccountCheckResult 添加参数

--- src/main/java/com/youzan/open/sdk/gen/v1_0_0/model/YouzanUsersAccountCheckResult.java
+++ src/main/java/com/youzan/open/sdk/gen/v1_0_0/model/YouzanUsersAccountCheckResult.java
@@ -1,20 +1,16 @@
 package com.youzan.open.sdk.gen.v1_0_0.model;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
-import com.youzan.open.sdk.api.APIParams;
-import com.youzan.open.sdk.api.FileParams;
 import com.youzan.open.sdk.api.APIResult;
-import com.youzan.open.sdk.model.ByteWrapper;
-import java.util.List;
-import java.util.Map;
-import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class YouzanUsersAccountCheckResult implements APIResult {
 
+    private boolean exist;
 
+    public boolean isExist() {
+        return exist;
+    }
 
-
+    public void setExist(boolean exist) {
+        this.exist = exist;
+    }
 }
